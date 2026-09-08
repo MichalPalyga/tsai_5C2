0426496b511dc7240849336fba2bc54daa21415c
MichalPalyga <michal11.atlas@o2.pl>
2026-09-08 11:13:34 +0200

    wprowadzenie

---
+++ Lekcja1-wprowadzenie/tablica.js
@@ -0,0 +1,15 @@
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s"];
+// console.log(alphabet.length)
+// console.log(alphabet[0])
+for(let i=0; i<alphabet.length;i++){
+    console.log(alphabet[i])
+}   
+
+// let table = [
+//     [
+//         [[1], [2], [3]], 
+//         [[4], [5], [6]], 
+//         [[7], [8], [9]]],
+//     [[], [], []],
+//     [[], [], []]
+// ]
